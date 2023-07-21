550a53cc9a6dd0f417803922c377db00a47ce87c
Artur lukashchuk <lukashchuk.artur2020@gmail.com>
2023-07-21 13:57:30 +0300

    rurik

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+<header> 
+  <img src="https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_2x_icon_124_40_292e71bcb52a56e2a9005164118f183b.png">
+</header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+img { justify-self: end; }
+
+</style>
+  
